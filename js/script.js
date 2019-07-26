3693a46ac008508ca5a1a968cb0fa491f23d10e8
Tobeyyyy <tobey199@gmail.com>
2019-07-26 10:47:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.onbeforeunload;//return to top when refresh page
 //set image cover to clear after 5s
 setTimeout(function(){
     //hide cover page to bottom
-	$('img#cover').css('opacity','0');
-	$('img#cover').css('index','-1');
+	$('img#cover').css({'opacity':'0','index':'-1'});
+	
 }, 5000);
 
