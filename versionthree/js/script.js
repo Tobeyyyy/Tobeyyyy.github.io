ce919acf3e783aed0154a61aa8d102b859a7103c
Eva Zhang <eva@Evas-MacBook-Pro.local>
2020-03-21 13:49:30 -0400

    versionthree

--- versionthree/js/script.js
+++ versionthree/js/script.js
@@ -1,12 +1,2 @@
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
-window.onbeforeunload;//return to top when refresh page
 
-//set image cover to clear after 5s
-setTimeout(function(){
-    //hide cover page to bottom
-	$('img#cover').css("display","none");
-	
-}, 5000);
 
