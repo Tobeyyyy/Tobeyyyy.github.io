ca6605b7a853dcbe7b031a1b90f486fc46097151
Eva Zhang <eva@evas-mbp.home>
2020-03-25 15:05:52 -0400

    versionthree

--- versionthree/js/script.js
+++ versionthree/js/script.js
@@ -1,8 +1,20 @@
 function showvideo(x){
 	var thistarget=x.className;
 	if (thistarget.indexOf('netsuite-inventory-transfer')){
-$('#netsuite-project-1').css('display','block');
+		if ($('#netsuite-project-1').css('display')=='none'){
+					$('#netsuite-project-1').css('display','block');
+			}else{
+				$('#netsuite-project-1').css('display','none');
+			}
 	}
 	console.log(x.className);
 }
 
+function alertvideo(){
+	if ($('p.showandhide').css('display')=='none'){
+		$('p.showandhide').css('display','block');
+	}else{
+		$('p.showandhide').css('display','none');
+
+	}
+}
