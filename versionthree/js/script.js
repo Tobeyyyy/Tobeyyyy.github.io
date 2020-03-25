6c94a01e90eeaadcfb183b09241d6a68635808f5
Eva Zhang <eva@evas-mbp.home>
2020-03-25 14:27:27 -0400

    versionthree

--- versionthree/js/script.js
+++ versionthree/js/script.js
@@ -1,4 +1,8 @@
 function showvideo(x){
+	var thistarget=x.className;
+	if (thistarget.indexOf('netsuite-inventory-transfer')){
+		$('#netsuite-project-1').style.display='block';
+	}
 	console.log(x.className);
 }
 
